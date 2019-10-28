66fc301fb77bdf84421822bb1cadb330014a9d34
i060847 <shachar.soel@sap.com>
2019-10-28 14:45:27 +0200

    validations package

---
+++ packages/validation/README.md
@@ -0,0 +1,40 @@
+[![npm (scoped)](https://img.shields.io/npm/v/@xml-tools/validation.svg)](https://www.npmjs.com/package/@xml-tools/validation)
+
+# @xml-tools/validation
+
+APIs which assist in implementing Validations / Diagnostics logic for XML.
+
+## Installation
+
+With npm:
+
+- `npm install @xml-tools/validation`
+
+With Yarn
+
+- `yarn add @xml-tools/validation`
+
+## Usage
+
+Please see the [TypeScript Definitions](./api.d.ts) for full API details.
+
+A simple usage example:
+
+```javascript
+const { validate } = require("xml-tools/validation");
+```
+
+## Support
+
+Please open [issues](https://github.com/SAP/xml-tols/issues) on github.
+
+## Contributing
+
+See [CONTRIBUTING.md](./CONTRIBUTING.md).
+
+## License
+
+Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the [LICENSE file](../../LICENSE).
+
+[ast]: https://en.wikipedia.org/wiki/Abstract_syntax_tree
---
+++ packages/validation/test/validate-apis-spec.js
@@ -0,0 +1,62 @@
+const { expect } = require("chai");
+const { map } = require("lodash");
+const { parse } = require("@xml-tools/parser");
+const { buildAst } = require("@xml-tools/ast");
+const { validate } = require("../");
+
+describe("The XML Validations APIs", () => {
+  it("can validate attributes", () => {
+    const xmlText = `<note>
+    <to>Bill</to>
+    <from>Tim</from>
+    </note>`;
+
+    const ast = xmlTextToAst(xmlText);
+
+    const issues = validate({
+      doc: ast,
+      validators: {
+        element: [
+          node => {
+            return { msg: node.name };
+          }
+        ]
+      }
+    });
+
+    const msgNodeNames = map(issues, _ => _.msg);
+    expect(msgNodeNames).to.have.lengthOf(3);
+    expect(msgNodeNames).to.have.members(["note", "to", "from"]);
+  });
+
+  it("can validate elements", () => {
+    const xmlText = `<note clearance="top-secret">
+    <to hidden="true">Bill</to>
+    <to>Gates</to>
+    <from>Tim</from>
+    </note>`;
+
+    const ast = xmlTextToAst(xmlText);
+
+    const issues = validate({
+      doc: ast,
+      validators: {
+        attribute: [
+          node => {
+            return { msg: node.key };
+          }
+        ]
+      }
+    });
+
+    const msgNodeNames = map(issues, _ => _.msg);
+    expect(msgNodeNames).to.have.lengthOf(2);
+    expect(msgNodeNames).to.have.members(["clearance", "hidden"]);
+  });
+});
+
+function xmlTextToAst(text) {
+  const { cst } = parse(text);
+  const ast = buildAst(cst);
+  return ast;
+}
