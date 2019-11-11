bd5a3431bf4602959ed1e60c1860c1f5eae4726b
i060847 <shachar.soel@sap.com>
2019-11-11 18:27:09 +0200

    test(parser): remove invalid snapshot tests

--- packages/parser/test/snapshots/invalid/attributes-no-value-last/sample.spec.js
+++
@@ -1,7 +0,0 @@
-const {
-  executeInValidSampleTest,
-  testNameFromDir
-} = require("../../../sample-test");
-describe(`${testNameFromDir(__dirname)}`, () => {
-  executeInValidSampleTest(__dirname);
-});
--- packages/parser/test/snapshots/invalid/attributes-no-value-no-eql-middle/sample.spec.js
+++
@@ -1,9 +0,0 @@
-const {
-  executeInValidSampleTest,
-  testNameFromDir
-} = require("../../../sample-test");
-
-// TODO: should we disable single token deletion in this case?
-describe(`${testNameFromDir(__dirname)}`, () => {
-  executeInValidSampleTest(__dirname);
-});
