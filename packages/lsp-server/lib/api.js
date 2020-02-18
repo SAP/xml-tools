7cefba537244de7cfcd55a27a74209ad56f59587
I060847 <shachar.soel@sap.com>
2020-02-18 20:21:50 +0200

    refactor(lsp-server): more robust manner to expose server "main" path

--- packages/lsp-server/lib/api.js
+++ packages/lsp-server/lib/api.js
@@ -1,6 +1,6 @@
-const { resolve } = require("path");
-
-const SERVER_PATH = resolve(__dirname, "server.js");
+// require.resolve will throw an error if the module does not exist
+// thus ensuring a valid path is provided (e.g if server.js is renamed).
+const SERVER_PATH = require.resolve("./server.js");
 
 module.exports = {
   SERVER_PATH: SERVER_PATH
