dc1645c883228d7334c03143715b62f02d3073de
I060847 <shachar.soel@sap.com>
2020-02-18 19:48:37 +0200

    refactor(lsp-server): simplify language-service helpers
    - No need to use "currying" pattern: Document => Error => Diagnostic
      we don't do any real composition here, a simpler signature:
      (Document, Error) => Diagnostic.
    - No need to dynamically create new functions for every single document.
    - No need to assign functions to variables, simple functions should suffice in this case.
    
    simple (Document, Error) => Diagnostic is sufficent

--- packages/lsp-server/lib/language-services.js
+++ packages/lsp-server/lib/language-services.js
@@ -1,39 +1,56 @@
 const { DiagnosticSeverity, Range } = require("vscode-languageserver");
+const { map } = require("lodash");
 const { parse } = require("@xml-tools/parser");
 
 const SYNTAX_ERROR_MSG = "Syntax error";
 
-const lexingErrorToDiagnostic = document => error => ({
-  message: error.message,
-  range: Range.create(
-    document.positionAt(error.offset),
-    document.positionAt(error.offset + error.length)
-  ),
-  severity: DiagnosticSeverity.Error,
-  source: SYNTAX_ERROR_MSG
-});
-
-const parsingErrorToDiagnostic = document => error => ({
-  message: error.message,
-  range: {
-    start: document.positionAt(error.token.startOffset),
-    end: document.positionAt(error.token.endOffset ? error.token.endOffset : 0)
-  },
-  severity: DiagnosticSeverity.Error,
-  source: SYNTAX_ERROR_MSG
-});
+/**
+ * @param {TextDocument} document
+ * @param {import("chevrotain").ILexingError} error
+ * @returns {import("vscode-languageserver-types").Diagnostic}
+ */
+function lexingErrorToDiagnostic(document, error) {
+  return {
+    message: error.message,
+    range: Range.create(
+      document.positionAt(error.offset),
+      document.positionAt(error.offset + error.length)
+    ),
+    severity: DiagnosticSeverity.Error,
+    source: SYNTAX_ERROR_MSG
+  };
+}
 
 /**
  * @param {TextDocument} document
- * @returns {Diagnostic[]}
+ * @param {import("chevrotain").ILexingError} error
+ * @returns {import("vscode-languageserver-types").Diagnostic}
+ */
+function parsingErrorToDiagnostic(document, error) {
+  return {
+    message: error.message,
+    range: {
+      start: document.positionAt(error.token.startOffset),
+      end: document.positionAt(
+        error.token.endOffset ? error.token.endOffset : 0
+      )
+    },
+    severity: DiagnosticSeverity.Error,
+    source: SYNTAX_ERROR_MSG
+  };
+}
+
+/**
+ * @param {TextDocument} document
+ * @returns {import("vscode-languageserver-types").Diagnostic[]}
  */
 async function validateDocument(document) {
   let diagnostics = [];
   if (document.languageId === "xml") {
     const { lexErrors, parseErrors } = parse(document.getText());
     diagnostics = [
-      ...lexErrors.map(lexingErrorToDiagnostic(document)),
-      ...parseErrors.map(parsingErrorToDiagnostic(document))
+      ...map(lexErrors, _ => lexingErrorToDiagnostic(document, _)),
+      ...map(parseErrors, _ => parsingErrorToDiagnostic(document, _))
     ];
   }
   return diagnostics;
