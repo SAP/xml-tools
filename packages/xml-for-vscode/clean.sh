c91506ad50df528f1ae5dd4b4d8eed9726778ba0
Pol <sapir.pol@sap.com>
2020-03-05 18:36:15 +0200

    ci: xml-for-vscode

--- packages/xml-for-vscode/clean.sh
+++ packages/xml-for-vscode/clean.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Clean
-rm ../langauge-server/xml-tools-language-server.tgz
+rm ../language-server/xml-tools-language-server.tgz
 rm ../parser/xml-tools-parser.tgz
 rm -rf node_modules
 
 # Revert to original state
+git checkout package.json
 yarn
