c1279e0cbd9909f72a3b8f3cb7eb3fe70898ce3e
Pol <sapir.pol@sap.com>
2020-03-08 14:42:46 +0200

    ci(xml-for-vscode): update

---
+++ scripts/publish-lerna.js
@@ -0,0 +1,19 @@
+const { spawnSync } = require("child_process").exec;
+
+const circleTag = process.env["CIRCLE_TAG"];
+const isReleaseTag = /RELEASE/.test(circleTag);
+
+if (isReleaseTag) {
+  // spawn sync process that does "lerna publish --yes"
+  const result = spawnSync("lerna", ["publish", "from-git", "--yes"], {
+    cwd: process.cwd(),
+    env: process.env,
+    stdio: "pipe",
+    encoding: "utf-8"
+  });
+
+  const savedOutput = result.stdout;
+  console.log(String(savedOutput));
+} else {
+  console.log("skipping publish to npm, because not running under RELEASE tag");
+}
