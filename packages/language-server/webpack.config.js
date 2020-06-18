6ea17ac96e63da6df8321a00007c63f69d9bcdf0
Shahar Soel <shahars@protonmail.com>
2020-06-18 14:22:21 +0300

    build: bundling of sources in VSIX

---
+++ packages/language-server/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const baseConfig = require("../../webpack.config.base");
+
+const config = {
+  ...baseConfig,
+  entry: "./lib/server.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "server.js",
+    libraryTarget: "commonjs2",
+    devtoolModuleFilenameTemplate: "../[resource-path]",
+  },
+};
+module.exports = config;
---
+++ packages/xml-toolkit/scripts/package-vsix.js
@@ -0,0 +1,90 @@
+/* istanbul ignore file */
+/**
+ * Workaround to: https://github.com/microsoft/vscode-vsce/issues/300
+ * This "sorts of" implements the (broken) `yarn list` with support for workspaces
+ * by hot-patching VSCE cli tool in combination with yarn's workspaces `nohoist` option.
+ *
+ * See code comments for details.
+ *
+ * Possible disadvantages:
+ * - Some dev artifacts (e.g coverage reports) may be included in the VSIX.
+ * - Need to ensure assumptions this logic relies on, (e.g nohoist configuration details).
+ * - Could break when VSCE dep version changes.
+ */
+const proxyquire = require("proxyquire");
+const { expect } = require("chai");
+const { resolve } = require("path");
+const { forEach } = require("lodash");
+const { readFileSync, writeFileSync } = require("fs");
+const { writeJsonSync } = require("fs-extra");
+
+const extensionRootPkg = require("../package.json");
+const monoRepoRootPkg = require("../../../package.json");
+
+const extName = extensionRootPkg.name;
+const monoRepoNoHoist = monoRepoRootPkg.workspaces.nohoist;
+
+// ensure nohoist is configured correctly so the `language-server` dependency
+// of the VSCode extension would be present in the extensions's **own** node_modules dir.
+// - https://classic.yarnpkg.com/blog/2018/02/15/nohoist/
+forEach(["@xml-tools/language-server"], (_) => {
+  // Shallow
+  expect(
+    monoRepoNoHoist,
+    `Add "${extName}/${_}" to root monorepo package.json[workspaces.nohoist]`
+  ).to.include(`${extName}/${_}`);
+  // Transitive
+  expect(
+    monoRepoNoHoist,
+    `Add "${extName}/${_}/**" to root monorepo package.json[workspaces.nohoist]`
+  ).to.include(`${extName}/${_}/**`);
+});
+
+// The path to the language server must be resolved from **inside** the VSCode Ext's node_modules.
+const langServerDir = resolve(
+  __dirname,
+  "..",
+  "node_modules",
+  "@xml-tools",
+  "language-server"
+);
+
+// **Hot-Patching** VSCE using proxyquire.
+const rootPkgDir = resolve(__dirname, "..");
+const getDepsStub = {
+  getDependencies: async () => [rootPkgDir, langServerDir],
+};
+const { packageCommand } = proxyquire("vsce/out/package", {
+  "./npm": getDepsStub,
+});
+
+const pkgJsonPath = resolve(rootPkgDir, "package.json");
+// Read & save the original literal representation of the pkg.json
+// To avoid dealing with re-formatting (prettier) later on.
+const pkgJsonOrgStr = readFileSync(pkgJsonPath, "utf8");
+const pkgJson = JSON.parse(pkgJsonOrgStr);
+// During development flows the `main` should point to the compiled sourced
+// for fast dev feedback loops.
+expect(pkgJson.main).to.equal("./lib/extension");
+// During production flows the main should point to the bundled sources
+// to reduce loading time.
+pkgJson.main = "./dist/extension";
+writeJsonSync(pkgJsonPath, pkgJson, { spaces: 2, EOF: "\n" });
+
+packageCommand({
+  cwd: rootPkgDir,
+  packagePath: undefined,
+  baseContentUrl: undefined,
+  baseImagesUrl: undefined,
+  useYarn: true,
+  ignoreFile: undefined,
+  expandGitHubIssueLinks: undefined,
+})
+  .catch((e) => {
+    console.error(e.message);
+    process.exitCode = 666;
+  })
+  .finally(() => {
+    // revert changes to the pkg.json, ensure clean git working directory
+    writeFileSync(pkgJsonPath, pkgJsonOrgStr);
+  });
