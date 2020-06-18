c82937a4e6182eefea221ced2b0c8979108d8547
Shahar Soel <4233843+bd82@users.noreply.github.com>
2020-06-18 14:33:50 +0300

    build: bundling of sources in VSIX (#205)

--- packages/language-server/lib/api.js
+++ packages/language-server/lib/api.js
@@ -1,7 +1,26 @@
-// require.resolve will throw an error if the module does not exist
-// thus ensuring a valid path is provided (e.g if server.js is renamed).
-const SERVER_PATH = require.resolve("./server.js");
+const { existsSync } = require("fs");
+const { resolve } = require("path");
+
+// for use in productive flows
+const bundledPath = resolve(__dirname, "..", "dist", "server.js");
+// for dev flows
+const sourcesPath = resolve(__dirname, "server.js");
+
+// We assume that if the `node_modules` directory exists then we are running
+// in development mode, We rely on the fact that the bundled .vsix file of the
+// VSCode extension excludes the this package's `node_modules` folder (among others).
+// Note this logic would not work with yarn 2.0 / npm 8 / pnpm as `node_modules`
+// is no longer created by those package managers.
+const isDevelopmentRun = existsSync(resolve(__dirname, "..", "node_modules"));
+
+let serverPath;
+/* istanbul ignore else - no tests (yet?) on bundled artifacts */
+if (isDevelopmentRun) {
+  serverPath = sourcesPath;
+} else {
+  serverPath = bundledPath;
+}
 
 module.exports = {
-  SERVER_PATH: SERVER_PATH,
+  SERVER_PATH: serverPath,
 };
